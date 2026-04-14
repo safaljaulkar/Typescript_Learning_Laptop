0555303f1e9b9e222b8a3d8a783f7a706c9b19d9
safaljaulkar <167606868+safaljaulkar@users.noreply.github.com>
2026-04-14 19:04:26 +0530

    committed new changes

--- tests/test3.spec.ts
+++ tests/test3.spec.ts
@@ -1,1 +1,19 @@
-import{test, expect} from '@playwright/tests'
+
+
+
+
+let beforecount = 30;
+let aftercount = 20;
+
+if(beforecount> aftercount){
+    await expect (beforecount).toBeGreaterThan(aftercount)
+
+else if{
+
+  await expect(beforecount).toBeLessThan(aftercount )
+}
+}
+
+
+
+        
